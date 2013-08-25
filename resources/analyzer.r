26e4c3bc9149f1af55ed3f6184e0c719dc44c3c0
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-08-25 13:35:21 -0300

    added time comparison for references

--- resources/analyzer.r
+++ resources/analyzer.r
@@ -6,33 +6,29 @@ library(car)
 scores <- read.csv("/Users/guilespi/Documents/Development/interrupted/bioinformatics/bio1/multiple_seq_aligner/results/scores.csv")
 #omit lines where bali score failed
 scores <- na.omit(scores)
-#segmentation of 
+#segmentation of balibase references
 ref1 <- subset(scores, scores$referid == "ref1")
 ref2 <- subset(scores, scores$referid == "ref2")
 ref3 <- subset(scores, scores$referid == "ref3")
 ref4 <- subset(scores, scores$referid == "ref4")
 ref5 <- subset(scores, scores$referid == "ref5")
+ref7 <- subset(scores, scores$referid == "ref7")
+ref8 <- subset(scores, scores$referid == "ref8")
 
-par(mfrow=c(1,3))
-bwplot(tc ~ algorithm, data = ref2)
+
 #comparison of scores for the different references
-bwplot(algorithm~sp, data=ref1, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score")
-bwplot(algorithm~sp, data=ref2, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score")
-bwplot(algorithm~sp, data=ref3, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score")
-bwplot(algorithm~sp, data=ref4, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score")
-bwplot(algorithm~sp, data=ref5, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score")
+bwplot(algorithm~sp, data=ref1, panel=panel.bwplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref1")
+bwplot(algorithm~sp, data=ref2, panel=panel.bwplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref2")
+bwplot(algorithm~sp, data=ref3, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref3")
+bwplot(algorithm~sp, data=ref4, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref4")
+bwplot(algorithm~sp, data=ref5, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref5")
+bwplot(algorithm~sp, data=ref7, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref7")
+bwplot(algorithm~sp, data=ref8, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="score", main="results ref8")
+
 #comparison of execution times
-bwplot(algorithm~time, data=ref1, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución")
-bwplot(algorithm~time, data=ref2, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución")
-bwplot(algorithm~time, data=ref3, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución")
-bwplot(algorithm~time, data=ref4, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución")
-bwplot(algorithm~time, data=ref5, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución")
-
-
-scatterplot(sp~tc, data=ref1)
-
-qqPlot(ref1$time, main="Normal QQ plot de temperatura")
-ref1.poa = subset(ref1, ref1$algorithm == "poa")
-plot(density(ref1.poa$sp, na.rm=T))
-ref1.clustalw2  = subset(ref1, ref1$algorithm == "clustalw2")
-lines(density(ref1.clustalw2$sp, na.rm=T))
+bwplot(algorithm~time, data=ref1, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref1")
+bwplot(algorithm~time, data=ref2, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref2")
+bwplot(algorithm~time, data=ref3, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref3")
+bwplot(algorithm~time, data=ref4, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref4")
+bwplot(algorithm~time, data=ref5, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref5")
+bwplot(algorithm~time, data=ref8, panel=panel.bpplot, datadensity=TRUE,ylab="algoritmo", xlab="tiempo de ejecución", main="times ref8")
